4b4d94df6922a539199fbcc6cf86d805a5f302a6
Thércio Rodrigues <thercior@gmail.com>
2024-11-13 20:34:09 -0300

    :bug: :lipstick: fix/style: corrigido bug do button de recolher/expandir sidebar, destaque de chats e efeito de expandir/recolher finalizado

--- static/js/toggleSidebar.js
+++ static/js/toggleSidebar.js
@@ -1,8 +1,14 @@
 document.addEventListener("DOMContentLoaded", function() {
     const sidebar = document.getElementById("sidebar");
     const toggleSidebar = document.getElementById("toggleSidebar");
+    const mainContent = document.getElementById("mainContent");
 
-    toogleSidebar.addEventListener("click", () => {
+    console.log("Script carregado");
+    toggleSidebar.addEventListener("click", () => {
+        console.log("Botão clicado");
         sidebar.classList.toggle("-translate-x-full");
+        toggleSidebar.classList.toggle("bg-gray-800");
+        mainContent.classList.toggle("-ml-52");
+        console.log("Classes da sidebar:", sidebar.classList);
     });
 });
