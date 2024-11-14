77ab39bf4421e0f8f72ef2777b834686e370d5ea
Thércio Rodrigues <thercior@gmail.com>
2024-11-13 21:38:28 -0300

    :sparkles: :bug: feat/fix: implentado template de alteração de perfil de usuário e corrigido bug no cadastro de usuário

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
-from accounts.views import LoginView, LogoutView, RegisterView
+from accounts.views import LoginView, LogoutView, RegisterView, UpdateRegisterView
 
 
-app_name = 'Chatbot'
+app_name = 'Accounts'
 
 urlpatterns = [
     path('accounts/login/', LoginView.as_view(), name='login'),
     path('accounts/logout/', LogoutView.as_view(), name='logout'),
     path('accounts/cadastro/', RegisterView.as_view(), name='register'),
+    path('accounts/editar_perfil/<int:pk>', UpdateRegisterView.as_view(), name='update_profile')
 ]
--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.models import User
 from django.contrib.auth.views import LoginView, LogoutView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView
 from django.urls import reverse_lazy
+from accounts.forms import CustomUserCreationForm, UserProfileForm
+
 
 
 class LoginView(LoginView):
@@ -19,6 +21,16 @@ class LogoutView(LogoutView):
 
 
 class RegisterView(CreateView):
-    form_class = UserCreationForm
+    form_class = CustomUserCreationForm
     template_name = 'Accounts/register.html'
     success_url = reverse_lazy('Accounts:login')
+
+    def form_valid(self, form):
+        return super().form_valid(form)
+
+
+class UpdateRegisterView(LoginRequiredMixin, UpdateView):
+    model = User
+    form_class = UserProfileForm
+    template_name = 'Accounts/edit_profile.html'
+    success_url = reverse_lazy('Chatbot:conversation_list')
