c79a4cb5aafc8c130bca741f077b0aec06fd6a34
Thércio Rodrigues <thercior@gmail.com>
2024-11-20 22:18:11 -0300

    :lipstick: fix: tentativas de corrigir visualização e iteração do sidebar

--- static/js/toggleSidebar.js
+++ static/js/toggleSidebar.js
@@ -5,10 +5,45 @@ document.addEventListener("DOMContentLoaded", function() {
     const navbarContent = document.getElementById("navbarContent");
     const mobileNavbar = document.getElementById("mobileNavbar");
 
+
+
+    
+    //  define o breakpoints para mobile
+    const isMobile = () => window.innerWidth < 1024;
+
+    // toggleSidebar.addEventListener("click", () => {
+    //     sidebar.classList.toggle("-translate-x-full");
+    //     mainContent.classList.toggle("-ml-64");
+    //     mobileNavbar.classList.toggle("hidden");
+    // });
+
     toggleSidebar.addEventListener("click", () => {
-        sidebar.classList.toggle("-translate-x-full");
-        mainContent.classList.toggle("-ml-64");
-        mobileNavbar.classList.toggle("hidden");
+        if (isMobile()) {
+            sidebar.classList.toggle("-translate-x-full");
+            mainContent.classList.toggle("-ml-64");
+            mobileNavbar.classList.toggle("hidden");
+        } else {
+            sidebar.classList.toggle("-translate-x-full");
+            mainContent.classList.toggle("-ml-64");
+            // mobileNavbar.classList.toggle("hidden");
+        }
     });
+    
+    //  Recalcular ao redimensionar a tela:
+    // window.addEventListener("resize", () => {
+    //     if (!isMobile()) {
+    //         sidebar.classList.remove("-translate-x-full");
+    //         sidebar.classList.add("z-0");
+    //         mainContent.classList.remove("z-0");
+    //         mainContent.classList.add("z-5");
+    //         mobileNavbar.classList.toggle("hidden");
+    //         mainContent.classList.toggle("-ml-64");
+    //     } else {
+    //         sidebar.classList.remove("z-40");
+    //         sidebar.classList.add("-translate-x-full");
+    //         mobileNavbar.classList.toggle("hidden");
+    //         mainContent.classList.toggle("-ml-64");
+    //     }
+    // });
 
 });
