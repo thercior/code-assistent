8453cd89a84330e9471bfde75a2b0df76d007137
Thércio Rodrigues <thercior@gmail.com>
2024-11-23 11:51:28 -0300

    :lipstick: style/refactor: toggle sidebar com deslocamento do #mainContent para telas maiores e sobreposição ao #mainContet para mobiles

--- static/js/toggleSidebar.js
+++ static/js/toggleSidebar.js
@@ -5,44 +5,29 @@ document.addEventListener("DOMContentLoaded", function() {
     const navbarContent = document.getElementById("navbarContent");
     const mobileNavbar = document.getElementById("mobileNavbar");
 
-
-
-    
     //  define o breakpoints para mobile
     const isMobile = () => window.innerWidth < 1024;
 
-    // toggleSidebar.addEventListener("click", () => {
-    //     sidebar.classList.toggle("-translate-x-full");
-    //     mainContent.classList.toggle("-ml-64");
-    //     mobileNavbar.classList.toggle("hidden");
-    // });
-
     toggleSidebar.addEventListener("click", () => {
         if (isMobile()) {
-            sidebar.classList.toggle("-translate-x-full");
-            mainContent.classList.toggle("-ml-64");
-            mobileNavbar.classList.toggle("hidden");
+            sidebar.classList.toggle("open");
         } else {
             sidebar.classList.toggle("-translate-x-full");
             mainContent.classList.toggle("-ml-64");
-            // mobileNavbar.classList.toggle("hidden");
+            mobileNavbar.classList.toggle("hidden");
         }
     });
     
     //  Recalcular ao redimensionar a tela:
     // window.addEventListener("resize", () => {
     //     if (!isMobile()) {
-    //         sidebar.classList.remove("-translate-x-full");
-    //         sidebar.classList.add("z-0");
-    //         mainContent.classList.remove("z-0");
-    //         mainContent.classList.add("z-5");
-    //         mobileNavbar.classList.toggle("hidden");
-    //         mainContent.classList.toggle("-ml-64");
-    //     } else {
-    //         sidebar.classList.remove("z-40");
+    //         sidebar.classList.remove("open");
     //         sidebar.classList.add("-translate-x-full");
-    //         mobileNavbar.classList.toggle("hidden");
     //         mainContent.classList.toggle("-ml-64");
+    //         mobileNavbar.classList.toggle("hidden");
+    //     } else {
+    //         sidebar.classList.remove("-translate-x-full");
+    //         mainContent.style.marginLeft = "0";
     //     }
     // });
 
